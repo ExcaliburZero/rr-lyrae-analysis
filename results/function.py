33435084f1574bcfa565c09d91969016f3c11173
Christopher Wells <cwellsny@nycap.rr.com>
2016-12-17 17:53:39 -0500

    Create probability function graphs
    Create the probability function and make some graphs of it.

--- results/function.py
+++ results/function.py
@@ -1,9 +1,10 @@
 import math
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
-mu  = 0.5636773 # Mean
-row = 0.1076015 # Standard deviation
+mu  = 0.5445663 # Mean
+row = 0.1208535 # Standard deviation
 
 def f(x):
     """Population density function for RR Lyrae star periods."""
@@ -14,22 +15,30 @@ def g(x):
     return f(x) / f(mu)
 
 def main():
+    # Determine the graph file type
+    filetype = "pdf"
+    if len(sys.argv) == 2:
+        filetype = sys.argv[1]
+
     # Generate the (x, y) data
-    start = 0.2
-    end = 0.9
+    start = 0
+    end = 1
     interval = 0.005
-    xs = [x for x in np.arange(start, end, interval)]
+    xs = [x for x in np.arange(start, end + interval, interval)]
     results = [g(x) for x in xs]
 
     # Plot the data
     plt.plot(xs, results)
 
+    # Save the graph
+    plt.savefig("graph." + filetype)
+
     # Plot standard deviation lines
     for x in range(-3,4):
         plt.axvline(mu + (x * row), color="red")
 
-    # Show the graph
-    plt.show()
+    # Save the graph with standard deviation lines
+    plt.savefig("graph-lines." + filetype)
 
 if __name__ == '__main__':
     main()
